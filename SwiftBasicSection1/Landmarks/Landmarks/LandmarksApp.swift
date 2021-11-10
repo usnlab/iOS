411533ed738cfb27db95f0238c7adfd801d9db3f
Yup Kim <chungyup@naver.com>
2021-11-10 10:36:05 +0900

    initialization first

---
+++ SwiftBasicSection1/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Quik on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
